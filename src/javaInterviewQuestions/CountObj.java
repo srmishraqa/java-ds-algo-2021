36164fec33e97c225fedd4a13bae9da47a1fedab
Soumya Mishra <soumya_rm@outlook.com>
2021-08-03 20:33:33 +0530

    added a counter for Obj

---
+++ src/javaInterviewQuestions/CountObj.java
@@ -0,0 +1,21 @@
+package javaInterviewQuestions;
+
+public class CountObj {
+
+    static int objCount = 0;
+
+    public CountObj() {
+        objCount++;
+    }
+    public int getCurrentObjCount(){
+        return objCount;
+    }
+
+    public static void main(String[] args) {
+        CountObj ob1 = new CountObj();
+        CountObj ob2 = new CountObj();
+        System.out.println(ob1.getCurrentObjCount());
+        CountObj ob3 = new CountObj();
+        System.out.println(ob2.getCurrentObjCount());
+    }
+}
